708100d616ca4cbb966de148b98fdfe5092832e5
JunAhJang <jnuga5853@naver.com>
2022-04-07 15:57:13 +0900

    5주차 복습

--- quiz/week04_quiz/ex8.py
+++ quiz/week04_quiz/ex8.py
@@ -1,1 +1,6 @@
 # 아래의 지문을 똑같이 출력하는 프로그램 작성
+# import nltk
+# nltk.download('book',quiet=True)
+# from nltk.book import *
+
+print("과목명: 고급 파이썬 프로그래밍\\Advanced Python programming\n\"이 수업은 금요일에 진행됩니다.\"\n\n수업은 실시간 강의로 진행됩니다.\n\t- 질의 사항은 조교에게 메일로 보내주세요")
